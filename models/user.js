fd16ff9cfa63251a62c4151b56560d9d447ac855
Muhammad-Faisal-Mahmood <mfaisalm1000@gmail.com>
2024-02-03 05:25:42 +0500

    set up sign in

--- models/user.js
+++ models/user.js
@@ -33,18 +33,37 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", function (next) {
-    const user = this;
-    if (!user.isModified("password")) return next(); // Skip if password is not modified
-  
-    const salt = randomBytes(16).toString('hex'); // Generate a random salt
-    const hashedPassword = createHmac("sha256", salt)
-      .update(user.password)
-      .digest("hex");
-  
-    user.salt = salt; // Set the salt for the user
-    user.password = hashedPassword; // Set the hashed password
-    next();
-  });
-  
+  const user = this;
+  if (!user.isModified("password")) return next(); // Skip if password is not modified
+
+  const salt = randomBytes(16).toString("hex"); // Generate a random salt
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  user.salt = salt; // Set the salt for the user
+  user.password = hashedPassword; // Set the hashed password
+  next();
+});
+
+userSchema.static("matchPassword", async function (email, password) {
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw new Error("user not found");
+  }
+
+  const salt = user.salt;
+  const hashedPassword = user.password;
+
+  const userProvidedHash = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  if (hashedPassword !== userProvidedHash)
+    throw new Error("Incorrect password");
+
+  return { ...user, password: undefined, salt: undefined };
+});
+
 const User = model("user", userSchema);
 module.exports = User;
